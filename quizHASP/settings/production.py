70215744d213ebd23d6241fe7c8b585c79ecdf54
TRAMPlin30 <vicodin28@gmail.com>
2022-08-31 02:16:01 +0300

    rrr

--- quizHASP/settings/production.py
+++ quizHASP/settings/production.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'quizHASP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.joinpath('templates')],
+        'DIRS': (os.path.join(BASE_DIR, 'templates')),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
