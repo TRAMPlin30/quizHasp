9bd1988fbfc36893736a04b2e7b6018f008a1437
TRAMPlin30 <vicodin28@gmail.com>
2022-08-31 13:08:39 +0300

    second day

--- quizHASP/settings/production.py
+++ quizHASP/settings/production.py
@@ -137,7 +137,7 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / 'questions/static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
